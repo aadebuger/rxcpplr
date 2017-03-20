f27ddc8ec69c06be2d3797e12bf637a78a95ae3f
hua zhuang <zh@zqf.com.cn>
2017-03-20 17:25:42 +0800

    first commit

---
+++ consoletickermain.cpp
@@ -0,0 +1,17 @@
+#include "rxcpp/rx.hpp"
+#include <chronos>
+int main()
+{
+    auto scheduler = std::make_shared<rxcpp::EventLoopScheduler<>();
+    
+    auto ticker = rxcpp::Interval( std::chronos::milliseconds(250),scheduler);
+    
+    rxcpp::from(ticker)
+    .where([](int val) { return val % 2 == 0; })
+    .take(10)
+    .subscribe([](int val) {
+        std::cout << "tick " << val << std::endl;
+    });
+    
+    std::cout << "starting to tick" << std::endl;
+}
